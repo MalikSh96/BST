7953733321e12db5bf7388a1fddc97e2a9572922
Joklin <Joklin@10.50.137.124>
2018-02-13 13:23:31 +0100

    test added

---
+++ test/binarysearchtree/BinarySearchTreeTest.java
@@ -0,0 +1,44 @@
+package binarysearchtree;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class BinarySearchTreeTest {
+    
+    public BinarySearchTreeTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void testMain() {
+    }
+
+    @Test
+    public void testGet() {
+    }
+
+    @Test
+    public void testContainsKey() {
+    }
+
+    @Test
+    public void testPut() {
+    }
+
+    @Test
+    public void testRemoveNode() {
+    }
+
+    @Test
+    public void testKeys() {
+    }
+
+    @Test
+    public void testSize() {
+    }
+    
+}
